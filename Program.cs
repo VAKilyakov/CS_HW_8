fc1a41f951e0e2e4ef95feb17d48e53eac10f162
VAKilyakov <vakilyakov@bk.ru>
2023-07-30 18:10:42 +0300

    Task 2 is done v1

--- Program.cs
+++ Program.cs
@@ -91,3 +91,94 @@ PS D:\Home_Works\CS_HW_8> dotnet run
 9 5 3 2
 8 4 4 2
 */
+
+/* Задача 2: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+/* Решение 2
+int InputInteNum(string message) //ввод целочисленных чисел
+{
+    Console.Write(message + " ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] CreateArray2D (int Length, int Height) //ввод двумерного массива
+{
+    int[,] Array2D = new int[Height,Length];
+    for (int i=0; i < Height; i++)
+    {
+        for (int j=0; j<Length; j++)
+        {
+            Array2D[i,j] = InputInteNum("Введите значение массива с индексом [" + i + "," + j + "]");
+        }
+    }
+    return Array2D;
+}
+
+int[] SumNumRow (int[,] Array2D)
+{
+    int[] Array1D = new int[Array2D.GetLength(1)];
+    for (int i=0; i < Array2D.GetLength(0); i++) 
+    {
+        int NumSum = 0;
+        for (int j=0; j<Array2D.GetLength(1); j++) 
+        {
+            NumSum += Array2D[i,j];
+        }
+        Array1D[i] =  NumSum;
+    }
+    return Array1D;
+}
+
+void PrintResult(int[] Array1D)
+{
+    int MinSumRow = Array1D[0];
+    int IndexRow = 0;
+    for(int i=1; i < Array1D.Length; i++)
+    {
+        if(MinSumRow>Array1D[i])
+        {
+            MinSumRow = Array1D[i];
+            IndexRow = i;
+        }
+    }
+    Console.Write("Строка с минимальным значением - " + (IndexRow+1));
+}
+
+int Height = InputInteNum("Введите количество строк в массиве");
+int Length = InputInteNum("Введите количество столбцов в массиве");
+int[,] Array2D = CreateArray2D(Length,Height);
+int[] Array1D = SumNumRow(Array2D);
+PrintResult(Array1D);
+*/
+
+/* Тест 2
+PS D:\Home_Works\CS_HW_8> dotnet run
+Введите количество строк в массиве 4
+Введите количество столбцов в массиве 4
+Введите значение массива с индексом [0,0] 1
+Введите значение массива с индексом [0,1] 4
+Введите значение массива с индексом [0,2] 7
+Введите значение массива с индексом [0,3] 2
+Введите значение массива с индексом [1,0] 5
+Введите значение массива с индексом [1,1] 9
+Введите значение массива с индексом [1,2] 2
+Введите значение массива с индексом [1,3] 3
+Введите значение массива с индексом [2,0] 8
+Введите значение массива с индексом [2,1] 4
+Введите значение массива с индексом [2,2] 2
+Введите значение массива с индексом [2,3] 4
+Введите значение массива с индексом [3,0] 5
+Введите значение массива с индексом [3,1] 2
+Введите значение массива с индексом [3,2] 6
+Введите значение массива с индексом [3,3] 7
+Строка с минимальным значением - 1
+*/
+
